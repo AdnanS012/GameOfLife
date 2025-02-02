3afc7d3ccb39d997501e25556508aa73e76856ed
adnan.shaikh_ftc <adnan.shaikh_ftc@external.swiggy.in>
2025-02-02 17:20:10 +0530

    Refactored code for GRID class and added test case

--- src/main/java/org/example/Grid.java
+++ src/main/java/org/example/Grid.java
@@ -1,5 +1,28 @@
 package org.example;
 
+import java.util.Random;
+
 public class Grid {
+    private final int rows;
+    private final int cols;
+    private Cell[][] grid;
+
+    public Grid(int rows,int cols,double seedPercentage){
+        this.rows = rows;
+        this.cols = cols;
+        this.grid = new Cell[rows][cols];
+        seedGrid(seedPercentage);
+    }
+
+    private void seedGrid(double seedPercentage){
+        Random random = new Random();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                grid[i][j] = new Cell(random.nextDouble() < seedPercentage);
+            }
+        }
+    }
+
+
 
 }
--- src/test/java/GridTest.java
+++ src/test/java/GridTest.java
@@ -1,2 +1,12 @@
+import org.example.Grid;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
 public class GridTest {
+    @Test
+    public void testGridInitialization() {
+        Grid grid = new Grid(5,5,0.5);
+        assertNotNull(grid);
+    }
 }
