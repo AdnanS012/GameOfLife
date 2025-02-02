c337203695dfd9563eeab15690c3379cfcd2f666
adnan.shaikh_ftc <adnan.shaikh_ftc@external.swiggy.in>
2025-02-02 18:04:57 +0530

    Created GameOfLife class and added test cases

--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -1,3 +1,30 @@
-public class GameOfLife {
+import org.example.Grid;
 
+import java.util.Scanner;
+
+public class GameOfLife {
+private final Grid grid;
+
+
+    public GameOfLife(int rows,int cols,double seedPercentage) {
+        this.grid = new Grid(rows,cols,seedPercentage);
+    }
+
+    public void start(){
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            grid.printGrid();
+            if(grid.isAllDead()){
+                System.out.println("All cells are dead. Game Over!");
+                break;
+            }
+            System.out.println("Press Enter to continue or type 'exit' to quit");
+            String input = scanner.next();
+            if (input.equalsIgnoreCase("exit")){
+                break;
+            }
+            grid.tick();
+        }
+        scanner.close();
+    }
 }
--- src/test/java/GameOfLifeTest.java
+++ src/test/java/GameOfLifeTest.java
@@ -5,7 +5,13 @@ import static org.junit.Assert.assertNotNull;
 public class GameOfLifeTest {
     @Test
     public void testGameOfLifeInitialization() {
-        GameOfLife game = new GameOfLife();
+        GameOfLife game = new GameOfLife(5,5,0.5);
+        assertNotNull(game);
+    }
+    @Test
+    public void testGameOfLifeStart(){
+        GameOfLife game = new GameOfLife(5,5,0.5);
+        game.start();
         assertNotNull(game);
     }
 
